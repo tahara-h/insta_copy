2b6f0ee5109f075c8c47fa52aa6ee3dd8bc93224
tahara hiroyasu <hiroyasu.tahara@epkotsoftware.co.jp>
2023-01-05 12:48:42 +0900

    データベースへの接続

--- htdocs/ImagePosting/html/dbConfig.php
+++ htdocs/ImagePosting/html/dbConfig.php
@@ -2,11 +2,19 @@
 
 $dbName = "mysql:host=insta_copy_db;dbname=imagePosting;charset=utf8";
 $userName = "root";
+$db_password = "root";
+
+#コンソールログにデバックログを出力するためのクラス
+function console_log($data){
+    echo '<script>';
+    echo 'console.log('.json_encode($data).')';
+    echo '</script>';
+  }
 
 try{
-    $db = new PDO($dbName, $userName);
-    var_dump("success");
+    $db = new PDO($dbName, $userName, $db_password);
+    console_log("DBの接続が出来ました。");
 } catch (\Throwable $th){
-    var_dump("失敗");
+    console_log("DBの接続に失敗しました。");
     exit();
 }
